2eba4dc7373022fa0da791d487a183b19c83551c
= <=>
2024-02-25 02:25:35 +0530

    new

--- emp_crud/settings.py
+++ emp_crud/settings.py
@@ -132,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+TIME_ZONE = 'Asia/Kolkata'
