15b206f06b3cfb64504c847818c33912c16fe573
= <=>
2024-02-25 02:35:31 +0530

    v1 cicd pipeline

--- emp_crud/settings.py
+++ emp_crud/settings.py
@@ -131,8 +131,4 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-TIME_ZONE = 'Asia/Kolkata'
+DEFAULT_AUTO_FIELD = 'dj
